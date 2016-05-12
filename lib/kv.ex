4dbaf44ba8ffcaabd056301d7bb7e572f69c4f39
y_jono <pek9dkl@gmail.com>
2016-05-12 20:15:18 +0900

    チュートリアル途中までの内容を保存した。
    http://elixir-lang.org/getting-started/mix-otp/supervisor-and-application.html#supervision-trees

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,15 +1,21 @@
 defmodule KV do
-  def start_link do
-    Task.start_link(fn -> loop(%{}) end)
+  use Application
+
+  def start(_type, _args) do
+    KV.Supervisor.start_link
   end
 
-  defp loop(map) do
-    receive do
-      {:get, key, caller} ->
-        send caller, Map.get(map, key)
-        loop(map)
-      {:put, key, value} ->
-        loop(Map.put(map, key, value))
-    end
-  end
+  #def start_link do
+  #  Task.start_link(fn -> loop(%{}) end)
+  #end
+
+  #defp loop(map) do
+  #  receive do
+  #    {:get, key, caller} ->
+  #      send caller, Map.get(map, key)
+  #      loop(map)
+  #    {:put, key, value} ->
+  #      loop(Map.put(map, key, value))
+  #  end
+  #end
 end
